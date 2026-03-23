7d42d5ec8bea295e979406c8554e32c497814ce4
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-03-23 23:57:40 +0530

    sequencer --- no changes(single port to dual port)

--- 4_RAM/2_DUAL_PORT_RAM/06_sequencer.sv
+++ 4_RAM/2_DUAL_PORT_RAM/06_sequencer.sv
@@ -1,9 +1,9 @@
 // UVM -- SEQUENCER
 
-class sr_sequencer extends uvm_sequencer #(sr_transaction);
+class sr_sequencer #(type T = sr_transaction) extends uvm_sequencer #(T);
   
   // factory registration
-  `uvm_component_utils(sr_sequencer)
+  `uvm_component_param_utils(sr_sequencer #(T))
   
   // default new constructor -- to allocate object(memory allocation)
   function new(string name = "sr_sequencer", uvm_component parent);	// getting arguments of class instance and parent name
