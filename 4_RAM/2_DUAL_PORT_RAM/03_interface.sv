060ccc0afb40574c31f96f29b0953cc40020b252
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-03-24 23:57:04 +0530

    interface

--- 4_RAM/2_DUAL_PORT_RAM/03_interface.sv
+++ 4_RAM/2_DUAL_PORT_RAM/03_interface.sv
@@ -1,18 +1,19 @@
 // UVM - INTERFACE
 
 interface dr_intf #(parameter WIDTH = 8,
-                    parameter DEPTH = 256,
-                    parameter MODE = 0) (input logic clk);
+                    parameter DEPTH = 256) (input logic clk);
   
-  logic 					en;
+  logic 					mode;		// mode0 - read first, mode1 - write first
   
   // port A
+  logic 					a_en;
   logic 					a_wr;
   logic [$clog2(DEPTH)-1:0] a_addr;
   logic [WIDTH-1:0] 		a_data_in;
   logic [WIDTH-1:0]			a_data_out;
   
   // port B
+  logic 					b_en;
   logic 					b_wr;
   logic [$clog2(DEPTH)-1:0] b_addr;
   logic [WIDTH-1:0] 		b_data_in;
@@ -21,22 +22,16 @@ interface dr_intf #(parameter WIDTH = 8,
   // clocking block
   
   // stimuli driving
-  clocking cb_drive @(negedge clk);
+  clocking cb_driver @(negedge clk);
     default input #1 output #2;
-    output en, a_wr, a_addr, a_data_in, b_wr, b_addr, b_data_in;
+    output a_en, a_wr, a_addr, a_data_in, b_en, b_wr, b_addr, b_data_in;
     input a_data_out, b_data_out;
   endclocking
   
   // sampling
-  clocking cb_sample @(posedge clk);
+  clocking cb_monitor @(posedge clk);
     default input #0;
-    input en, a_wr, a_addr, a_data_in, b_wr, b_addr, b_data_in, a_data_out, b_data_out;
+    input a_en, a_wr, a_addr, a_data_in, b_en, b_wr, b_addr, b_data_in, a_data_out, b_data_out;
   endclocking
-  
-  // modport
-  modport tb( 
-    clocking cb_drive, 
-    clocking cb_sample, 			// access through clocking block
-    input a_data_out, b_data_out);		// testbench
     
 endinterface
