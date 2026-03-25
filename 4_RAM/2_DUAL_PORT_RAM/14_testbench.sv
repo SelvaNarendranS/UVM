5b1179119511be3a05b94602baf1e9fe3cb3bd44
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-03-25 23:50:47 +0530

    testbench

--- 4_RAM/2_DUAL_PORT_RAM/14_testbench.sv
+++ 4_RAM/2_DUAL_PORT_RAM/14_testbench.sv
@@ -3,32 +3,39 @@
 `timescale 1ns/1ps
 
 `include "package.sv"
-`include "interface.sv"
+`include "dr_interface.sv"
+`include "sr_interface.sv"
 
 // uvm bootstrap 
-// import uvm_pkg :: *;		// sv package import directive	-- added in package file
 import dr_pkg :: *;			// importing dr package
 `include "uvm_macros.svh"	// uvm macros - compailer directive -- to access macros
 
 module dr_testbench;
   bit clk;
+  logic mode_signal;  // Add a 1-bit mode signal
   
+  sr_intf #(WIDTH, DEPTH) stb_vintf(clk);		// interface handle
   dr_intf #(WIDTH, DEPTH, MODE) tb_vintf(clk);		// interface handle
+  
+  // Assign mode value
+  assign mode_signal = MODE;
+  assign tb_vintf.mode = MODE;
   
   // instantation
   dual_port_ram #(.WIDTH(WIDTH),
-                  .DEPTH(DEPTH),
-                  .MODE(MODE))
+                  .DEPTH(DEPTH))
   dut(.clk		  (clk),
-      .en 		  (tb_vintf.en),
+      .mode		  (mode_signal),  // Use 1-bit signal instead of parameter
       
       // port A
+      .a_en 	  (tb_vintf.a_en),
       .a_wr		  (tb_vintf.a_wr),
       .a_addr	  (tb_vintf.a_addr),
       .a_data_in  (tb_vintf.a_data_in),
       .a_data_out (tb_vintf.a_data_out),
       
       // port B
+      .b_en 	  (tb_vintf.b_en),
       .b_wr		  (tb_vintf.b_wr),
       .b_addr	  (tb_vintf.b_addr),
       .b_data_in  (tb_vintf.b_data_in),
@@ -44,12 +51,8 @@ module dr_testbench;
   // configuration interface and calling run_test
   initial begin
     // interface configuration to database
-    uvm_config_db #(virtual dr_intf #(WIDTH, DEPTH, MODE)) :: set(null, 	// starting point
-                                                                  "*",	// visibility - all lower hierachy
-                                                                  "vintf",	// name
-                                                                  tb_vintf	// value
-                                                                 );
-    
+    uvm_config_db #(virtual dr_intf #(WIDTH, DEPTH, MODE)) :: set(null, "*", "dr_vintf", tb_vintf);
+    uvm_config_db #(virtual sr_intf #(WIDTH, DEPTH)) :: set(null, "*", "vintf", stb_vintf);
     
     // configuring ram mode into database   -- for scoreboards
     uvm_config_db #(int) :: set(null, "*", "ram_mode", MODE);
