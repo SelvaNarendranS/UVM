8a23c44b6da23ab6bedb9a336802dadc369570d5
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-04-09 23:44:04 +0530

    mid operation mod_n variable counter

--- 3_MOD_N_COUNTER/01.mod_n_counter.sv
+++ 3_MOD_N_COUNTER/01.mod_n_counter.sv
@@ -11,26 +11,28 @@ module mod_N_counter #(parameter WIDTH = 8) (		// a fixed counter size
 );
   
   wire [WIDTH-1:0] psnt_limit = (mod_n == 0) ? ({WIDTH{1'b1}}) : mod_n;		// loading current limit -- continuously 
+  wire out_of_range = (q >= psnt_limit);		// flag
   
   always @(posedge clk) begin
     if(reset) begin
       q <= 0;			
     end
-    else begin
-      if(enable) begin
-        if(up_down) begin		// up counting
-          if(q >= psnt_limit-1)
-            q <= 0;				// reset the counter
-          else
-            q <= q + 1;			// increment counter
-        end
+    else if(out_of_range && enable) begin		// mid time mon n out of range detection
+      q <= up_down ? 0 : (psnt_limit - 1);
+    end
+    else if(enable) begin
+      if(up_down) begin		// up counting
+        if(q >= psnt_limit-1)
+          q <= 0;				// reset the counter
+        else
+          q <= q + 1;			// increment counter
+      end
         
-        else begin 				// down counting
-          if(q == 0)
-            q <= psnt_limit-1;				// reset the counter
-          else
-            q <= q - 1;			// decrement counter
-        end
+      else begin 				// down counting
+        if(q == 0)
+          q <= psnt_limit-1;				// reset the counter
+        else
+          q <= q - 1;			// decrement counter
       end
     end
   end
