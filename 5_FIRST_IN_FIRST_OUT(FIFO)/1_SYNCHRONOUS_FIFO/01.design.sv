62841546258581a4469ddca1ce517e346cd64714
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-04-13 22:51:30 +0530

    design

---
+++ 5_FIRST_IN_FIRST_OUT(FIFO)/1_SYNCHRONOUS_FIFO/01.design.sv
@@ -0,0 +1,50 @@
+// Synchronous FIFO verification - design
+module sync_fifo #(parameter WIDTH = 8, 
+                   parameter DEPTH = 4) (
+  // system
+  input logic 				clk,
+  input logic				reset,
+  
+  // input ports
+  input logic				wr_enable,
+  input logic 				rd_enable,
+  input logic [WIDTH - 1:0] data_in,
+  
+  // output ports
+  output logic [WIDTH -1:0]	data_out,
+  output logic				full,
+  output logic				empty
+);
+  
+  // internal counter & memory
+  logic [WIDTH - 1:0] mem[DEPTH - 1:0];
+  logic [$clog2(DEPTH) - 1:0] wr_ptr;
+  logic [$clog2(DEPTH) - 1:0] rd_ptr;
+  
+  // Write and read condition
+  always @(posedge clk) begin
+    if(reset) begin			// reset condition
+      wr_ptr   <= 0;
+      data_out <= 0;
+      rd_ptr   <= 0;
+    end
+    else begin
+      // write
+      if(wr_enable && !full) begin
+      mem[wr_ptr] <= data_in;
+      wr_ptr <= wr_ptr + 1'b1;
+    end
+      
+      // read
+      if(rd_enable && !empty) begin
+      data_out <= mem[rd_ptr];
+      rd_ptr   <= rd_ptr + 1'b1;
+    end
+  end
+  
+  // full & empty condition
+  assign full  = (wr_ptr + 1'b1) == rd_ptr;
+  assign empty = wr_ptr == rd_ptr;
+endmodule  
+
+// https://www.edaplayground.com/x/eZ7s
