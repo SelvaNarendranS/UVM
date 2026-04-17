1f277812a3da8f82596a16a1f532332a5344df0e
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-04-17 23:38:54 +0530

    sequence

---
+++ 5_FIRST_IN_FIRST_OUT(FIFO)/1_SYNCHRONOUS_FIFO/05.sequence.sv
@@ -0,0 +1,52 @@
+// sequence
+
+`ifndef "SYNC_FIFO_SEQUENCE"
+`define "SYNC_FIFO_SEQUENCE"
+
+class sync_fifo_sequence extends uvm_sequence #(sync_fifo_transaction);
+  
+  // factory registration 
+  `uvm_object_utils(sync_fifo_transaction)
+  sync_fifo_transaction trans;		// transaction handle
+  
+  // default constructor
+  function new(string name = "sync_fifo_sequence");
+    super.new(name);
+  endfunction
+  
+  // generating stimulus to the dut
+  task body;
+    `uvm_info(get_type_name(), "Stimuli generation starts", UVM_LOW);
+    
+    for(int i = 0; i <= (DEPTH*2); i++) begin
+      trans = sync_fifo_transaction :: type_id :: create("trans");
+      
+      start_item(trans);
+      
+      if(!trans.randomize() with {
+        if(i == 0) reset == 1;
+        else {
+          if(i <= DEPTH) {		// full condition
+            reset == 0;
+            wr_enable == 1;
+            rd_enable == 0;
+          };
+          else if(i > (DEPTH+(DEPTH/2))) {		// read condition
+            reset == 0;
+            rd_enable == 1;
+          };
+          else reset == 0;			// other conditions
+        };
+      }) begin
+        `uvm_error(get_type_name(), "Randomization failed");
+      end
+      
+      finish_item(trans);
+      `uvm_info(get_type_name(), $sformatf("reset = %0d | wr_enable = %0d | rd_enable = %0d | data_in = %0d | data_out = %0d | full = %0d | empty = %0d", reset, wr_enable, rd_enable, data_in, data_out, full, empty), UVM_LOW);
+    end
+      
+    endtask
+  
+endclass
+
+`endif
