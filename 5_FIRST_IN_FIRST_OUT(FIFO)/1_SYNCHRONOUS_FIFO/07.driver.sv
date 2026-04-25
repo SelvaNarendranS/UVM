1a63f5d9fafc4845dec44644050162200629d15b
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-04-25 23:13:48 +0530

    driver

--- 5_FIRST_IN_FIRST_OUT(FIFO)/1_SYNCHRONOUS_FIFO/07.driver.sv
+++ 5_FIRST_IN_FIRST_OUT(FIFO)/1_SYNCHRONOUS_FIFO/07.driver.sv
@@ -20,7 +20,7 @@ class sync_fifo_driver extends uvm_driver #(sync_fifo_transaction);
     super.build_phase(phase);
     
     // getting values from database  --- by modport
-    if(!uvm_config_db #(virtual cnt_intf #(WIDTH, DEPTH)) :: get(this, "", "vintf", vintf))
+    if(!uvm_config_db #(virtual intf #(WIDTH, DEPTH)) :: get(this, "", "vintf", vintf))
       `uvm_fatal(get_type_name(), "Virtual interface missing");		// stop the simulation with warning
   
   endfunction
@@ -43,12 +43,12 @@ class sync_fifo_driver extends uvm_driver #(sync_fifo_transaction);
     
     @(vintf.drive_cb);
     
-    vintf.drive_cb.reset 	<= tr.reset;
-    vintf.drive_cb.wr_enable   <= tr.wr_enable;
-    vintf.drive_cb.rd_enable 	<= tr.rd_enable;
-    vintf.drive_cb.data_in  <= tr.data_in;
+    vintf.drive_cb.reset 	 <= tr.reset;
+    vintf.drive_cb.wr_enable <= tr.wr_enable;
+    vintf.drive_cb.rd_enable <= tr.rd_enable;
+    vintf.drive_cb.data_in   <= tr.data_in;
     
-    `uvm_info(get_type_name(), $sformatf("reset = %0d | wr_enable = %0d | rd_enable = %0d | data_in = %0d | data_out = %0d | full = %0d | empty = %0d", reset, wr_enable, rd_enable, data_in, data_out, full, empty), UVM_LOW);
+    `uvm_info(get_type_name(), $sformatf("reset = %0d | wr_enable = %0d | rd_enable = %0d | data_in = %0d | data_out = %0d | full = %0d | empty = %0d", tr.reset, tr.wr_enable, tr.rd_enable, tr.data_in, tr.data_out, tr.full, tr.empty), UVM_LOW);
   endtask
   
 endclass
