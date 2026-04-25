d61bfb43f0de0aa94bcef5d9d6130ba5d9987728
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-04-25 23:06:38 +0530

    sequence

--- 5_FIRST_IN_FIRST_OUT(FIFO)/1_SYNCHRONOUS_FIFO/05.sequence.sv
+++ 5_FIRST_IN_FIRST_OUT(FIFO)/1_SYNCHRONOUS_FIFO/05.sequence.sv
@@ -1,12 +1,12 @@
 // sequence
 
-`ifndef "SYNC_FIFO_SEQUENCE"
-`define "SYNC_FIFO_SEQUENCE"
+`ifndef SYNC_FIFO_SEQUENCE
+`define SYNC_FIFO_SEQUENCE
 
 class sync_fifo_sequence extends uvm_sequence #(sync_fifo_transaction);
   
   // factory registration 
-  `uvm_object_utils(sync_fifo_transaction)
+  `uvm_object_utils(sync_fifo_sequence)
   sync_fifo_transaction trans;		// transaction handle
   
   // default constructor
@@ -30,19 +30,20 @@ class sync_fifo_sequence extends uvm_sequence #(sync_fifo_transaction);
             reset == 0;
             wr_enable == 1;
             rd_enable == 0;
-          };
+          }
           else if(i > (DEPTH+(DEPTH/2))) {		// read condition
             reset == 0;
             rd_enable == 1;
-          };
+            wr_enable == 0;
+          }
           else reset == 0;			// other conditions
-        };
+        }
       }) begin
         `uvm_error(get_type_name(), "Randomization failed");
       end
       
       finish_item(trans);
-      `uvm_info(get_type_name(), $sformatf("reset = %0d | wr_enable = %0d | rd_enable = %0d | data_in = %0d | data_out = %0d | full = %0d | empty = %0d", reset, wr_enable, rd_enable, data_in, data_out, full, empty), UVM_LOW);
+      `uvm_info(get_type_name(), $sformatf("reset = %0d | wr_enable = %0d | rd_enable = %0d | data_in = %0d | data_out = %0d | full = %0d | empty = %0d", trans.reset, trans.wr_enable, trans.rd_enable, trans.data_in, trans.data_out, trans.full, trans.empty), UVM_LOW);
     end
       
     endtask
