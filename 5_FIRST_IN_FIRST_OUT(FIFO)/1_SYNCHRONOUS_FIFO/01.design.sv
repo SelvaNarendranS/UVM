b51bf5fb3fedbe1e79812f618ca664e5e4c80bce
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-04-25 22:36:01 +0530

    design

--- 5_FIRST_IN_FIRST_OUT(FIFO)/1_SYNCHRONOUS_FIFO/01.design.sv
+++ 5_FIRST_IN_FIRST_OUT(FIFO)/1_SYNCHRONOUS_FIFO/01.design.sv
@@ -18,8 +18,8 @@ module sync_fifo #(parameter WIDTH = 8,
   
   // internal counter & memory
   logic [WIDTH - 1:0] mem[DEPTH - 1:0];
-  logic [$clog2(DEPTH) - 1:0] wr_ptr;
-  logic [$clog2(DEPTH) - 1:0] rd_ptr;
+  logic [$clog2(DEPTH):0] wr_ptr;
+  logic [$clog2(DEPTH):0] rd_ptr;
   
   // Write and read condition
   always @(posedge clk) begin
@@ -31,20 +31,20 @@ module sync_fifo #(parameter WIDTH = 8,
     else begin
       // write
       if(wr_enable && !full) begin
-        mem[wr_ptr] <= data_in;
+        mem[wr_ptr[$clog2(DEPTH)-1:0]] <= data_in;
         wr_ptr <= wr_ptr + 1'b1;
       end
       
       // read
       if(rd_enable && !empty) begin
-        data_out <= mem[rd_ptr];
+        data_out <= mem[rd_ptr[$clog2(DEPTH)-1:0]];
         rd_ptr   <= rd_ptr + 1'b1;
       end
     end
   end
   
   // full & empty condition
-  assign full  = (wr_ptr + 1'b1) == rd_ptr;
+  assign full  = ((wr_ptr[$clog2(DEPTH)-1:0]) == rd_ptr[$clog2(DEPTH)-1:0]) && (wr_ptr[$clog2(DEPTH)] != rd_ptr[$clog2(DEPTH)]);
   assign empty = wr_ptr == rd_ptr;
     
 endmodule  
