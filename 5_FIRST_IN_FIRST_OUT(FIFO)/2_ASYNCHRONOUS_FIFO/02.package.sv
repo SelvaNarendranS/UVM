cbeb226650b2a86537ee1e413c2a9e23a275bc55
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-04-28 23:42:11 +0530

    package

---
+++ 5_FIRST_IN_FIRST_OUT(FIFO)/2_ASYNCHRONOUS_FIFO/02.package.sv
@@ -0,0 +1,29 @@
+// package 
+
+package async_fifo_pkg;
+  
+  // uvm bootstrap
+  `import uvm_pkg :: *;
+  `include "uvm_macros.svh"
+  
+  // parameter
+  parameter WIDTH = 8;
+  parameter DEPTH = 4;
+  
+  // including files -- bottom -> to  -> top
+  // objects
+  `include "transaction.sv"
+  `include "sequence.sv"
+  
+  // components
+  `include "sequencer.sv"
+  `include "driver.sv"
+  `include "monitor.sv"
+  `include "agent.sv"
+
+  `include "scoreboard.sv"
+  `include "coverage.sv"
+  `include "environment.sv"
+  `include "test.sv"
+  
+endpackage
