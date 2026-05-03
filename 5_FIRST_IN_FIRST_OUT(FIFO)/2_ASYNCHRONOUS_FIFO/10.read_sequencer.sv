28dde4ea7290b3011b8bc81e16fadea57f704145
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-05-03 23:48:37 +0530

    read_sequencer

---
+++ 5_FIRST_IN_FIRST_OUT(FIFO)/2_ASYNCHRONOUS_FIFO/10.read_sequencer.sv
@@ -0,0 +1,18 @@
+// read sequencer
+
+`ifndef ASYNC_FIFO_RD_SEQUENCER
+`define ASYNC_FIFO_RD_SEQUENCER
+
+class async_fifo_rd_sequencer extends uvm_sequencer #(async_fifo_transaction);
+  
+  // factory registration
+  `uvm_component_utils(async_fifo_rd_sequencer);
+  
+  // default constructor
+  function new(string name = "async_fifo_rd_sequencer", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+endclass
+
+`endif
