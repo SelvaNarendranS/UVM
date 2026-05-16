5e23dd4041afd4d4892650b72004a3b9017da804
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-05-16 23:31:58 +0530

    updated write sequence

--- 5_FIRST_IN_FIRST_OUT(FIFO)/2_ASYNCHRONOUS_FIFO/06.write_sequence.sv
+++ 5_FIRST_IN_FIRST_OUT(FIFO)/2_ASYNCHRONOUS_FIFO/06.write_sequence.sv
@@ -1,13 +1,13 @@
-// sequence
+// write sequence
 
 `ifndef ASYNC_FIFO_WR_SEQUENCE
 `define ASYNC_FIFO_WR_SEQUENCE
 
-class async_fifo_wr_sequence extends uvm_sequence #(async_fifo_transaction);
+class async_fifo_wr_sequence extends uvm_sequence #(async_fifo_wr_transaction);
   
   // factory registration 
   `uvm_object_utils(async_fifo_wr_sequence)
-  async_fifo_transaction trans;		// transaction handle
+  async_fifo_wr_transaction wr_trans;		// transaction handle
   
   // default constructor
   function new(string name = "async_fifo_wr_sequence");
@@ -19,36 +19,41 @@ class async_fifo_wr_sequence extends uvm_sequence #(async_fifo_transaction);
     `uvm_info(get_type_name(), "Write Stimuli generation starts", UVM_LOW);
     
     for(int i = 0; i < (DEPTH * 2); i++) begin
-      trans = async_fifo_transaction :: type_id :: create("trans");		// object allocation for each inputs
+      wr_trans = async_fifo_wr_transaction :: type_id :: create("wr_trans");		// object allocation for each inputs
       
-      start_item(trans);
+      start_item(wr_trans);
       
-       if(i == 0) begin
-         $display("---- RESET condition ----");
-         trans.rst_cnst.constraint_mode(1);			// constraint mode ON
-         trans.wr_cnst.constraint_mode(0);
-         trans.rd_cnst.constraint_mode(0);
-         randomization();							// value generation
-        end
+      if(i == 0) begin
+        $display("---- RESET condition ----");
+        wr_trans.rst_cnst.constraint_mode(1);			// constraint mode ON
+        wr_trans.wr_cnst.constraint_mode(0);
+        randomization();							// value generation
+      end
       
-        // full condition satisfy
-        else if(i < DEPTH + 1) begin
-          $display("---- FULL condition ----");
-          trans.rst_cnst.constraint_mode(0);			// constraint mode OFF
-          trans.wr_cnst.constraint_mode(1);
-          trans.rd_cnst.constraint_mode(0);
-          randomization();			
-        end
+      // full condition satisfy
+      else if(i < DEPTH + 1) begin
+        $display("---- FULL condition ----");
+        wr_trans.rst_cnst.constraint_mode(0);			// constraint mode OFF
+        wr_trans.wr_cnst.constraint_mode(1);
+        randomization();			
+      end
       
-      finish_item(trans);
-      `uvm_info(get_type_name(), $sformatf("%0s", convert2string()), UVM_LOW);
+      else begin
+        $display("Write Idle condition");
+        wr_trans.wr_rst     = 1'b1;
+        wr_trans.wr_en      = 1'b0;
+        wr_trans.data_in    = 1'b0;
+      end
+        
+      finish_item(wr_trans);
+      `uvm_info(get_type_name(), $sformatf("%0s", wr_trans.convert2string()), UVM_LOW);
       
     end
   
   endtask
   
   function void randomization();
-    if(!trans.randomize()) begin
+    if(!wr_trans.randomize()) begin
       `uvm_error(get_type_name(), "Randomization failed");
     end
   endfunction
