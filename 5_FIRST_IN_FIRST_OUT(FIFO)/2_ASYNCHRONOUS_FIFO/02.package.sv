daab5f60bc2558aafeffcab1883f2bf756bcb2b5
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-05-16 23:25:07 +0530

    package

--- 5_FIRST_IN_FIRST_OUT(FIFO)/2_ASYNCHRONOUS_FIFO/02.package.sv
+++ 5_FIRST_IN_FIRST_OUT(FIFO)/2_ASYNCHRONOUS_FIFO/02.package.sv
@@ -3,23 +3,38 @@
 package async_fifo_pkg;
   
   // uvm bootstrap
-  `import uvm_pkg :: *;
+  import uvm_pkg :: *;
   `include "uvm_macros.svh"
   
   // parameter
   parameter WIDTH = 8;
   parameter DEPTH = 4;
+
+  `uvm_analysis_imp_decl(_wr)
+  `uvm_analysis_imp_decl(_rd)
   
   // including files -- bottom -> to  -> top
   // objects
-  `include "transaction.sv"
-  `include "sequence.sv"
+  `include "write_transaction.sv"
+  `include "read_transaction.sv"
+
+  // component sequencer
+  `include "write_sequencer.sv"
+  `include "read_sequencer.sv"
+  `include "virtual_sequencer.sv"
+
+  // object - sequence 
+  `include "write_sequence.sv"
+  `include "read_sequence.sv"
+  `include "virtual_sequence.sv"
   
   // components
-  `include "sequencer.sv"
-  `include "driver.sv"
-  `include "monitor.sv"
-  `include "agent.sv"
+  `include "write_driver.sv"
+  `include "read_driver.sv"
+  `include "write_monitor.sv"
+  `include "read_monitor.sv"
+  `include "write_agent.sv"
+  `include "read_agent.sv"
 
   `include "scoreboard.sv"
   `include "coverage.sv"
