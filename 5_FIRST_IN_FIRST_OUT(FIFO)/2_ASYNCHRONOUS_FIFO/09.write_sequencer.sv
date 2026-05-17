6c5b7e9bd50224e610cde282e8233b943b1bf812
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-05-17 22:25:31 +0530

    write sequencer

--- 5_FIRST_IN_FIRST_OUT(FIFO)/2_ASYNCHRONOUS_FIFO/09.write_sequencer.sv
+++ 5_FIRST_IN_FIRST_OUT(FIFO)/2_ASYNCHRONOUS_FIFO/09.write_sequencer.sv
@@ -1,12 +1,12 @@
-//write sequencer
+// write sequencer
 
 `ifndef ASYNC_FIFO_WR_SEQUENCER
 `define ASYNC_FIFO_WR_SEQUENCER
 
-class async_fifo_wr_sequencer extends uvm_sequencer #(async_fifo_transaction);
+class async_fifo_wr_sequencer extends uvm_sequencer #(async_fifo_wr_transaction);
   
   // factory registration
-  `uvm_component_utils(async_fifo_wr_sequencer);
+  `uvm_component_utils(async_fifo_wr_sequencer)
   
   // default constructor
   function new(string name = "async_fifo_wr_sequencer", uvm_component parent);
