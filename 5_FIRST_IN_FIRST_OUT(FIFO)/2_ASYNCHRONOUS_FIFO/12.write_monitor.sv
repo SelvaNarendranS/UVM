353d0d4e50204306255352c387ea8ec50763147b
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-05-17 22:30:34 +0530

    write driver

--- 5_FIRST_IN_FIRST_OUT(FIFO)/2_ASYNCHRONOUS_FIFO/12.write_monitor.sv
+++ 5_FIRST_IN_FIRST_OUT(FIFO)/2_ASYNCHRONOUS_FIFO/12.write_monitor.sv
@@ -1,21 +1,17 @@
-// write monitor
+// write driver
 
-`ifndef ASYNC_FIFO_WR_MONITOR
-`define ASYNC_FIFO_WR_MONITOR
+`ifndef ASYNC_FIFO_WR_DRIVER_SV
+`define ASYNC_FIFO_WR_DRIVER_SV
 
-class async_fifo_wr_monitor extends uvm_monitor;
+class async_fifo_wr_driver extends uvm_driver #(async_fifo_wr_transaction);
   
-  // factory registration
-  `uvm_component_utils(async_fifo_rd_monitor)
-  async_fifo_wr_transaction wr_trans;				// transaction handle
+  `uvm_component_utils(async_fifo_wr_driver)		// factory registration
+  async_fifo_wr_transaction wr_trans;			// transaction handle
   
-  virtual intf #(WIDTH, DEPTH) vintf;	// virtual interface
+  virtual intf #(WIDTH, DEPTH) vintf;		// virtual interface handle
   
-  // analysis port
-  uvm_analysis_port #(async_fifo_wr_transaction) wr_analysis_port;
-  
-   // default constructor
-  function new(string name = "async_fifo_rd_monitor", uvm_component parent);
+  // default constructor
+  function new(string name = "async_fifo_wr_driver", uvm_component parent);
     super.new(name, parent);
   endfunction
   
@@ -23,34 +19,35 @@ class async_fifo_wr_monitor extends uvm_monitor;
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     
-    // getting values from database  --- from modport
+    // getting values from database  --- by modport
     if(!uvm_config_db #(virtual intf #(WIDTH, DEPTH)) :: get(this, "", "vintf", vintf))
       `uvm_fatal(get_type_name(), "Virtual interface missing");		// stop the simulation with warning
-    
-    wr_analysis_port = new("analysis_port", this);
+  
   endfunction
   
   // run phase
   task run_phase(uvm_phase phase);
+    
     forever begin
-      wr_trans = sync_fifo_wr_transaction :: type_id :: create("wr_trans");		// object allocation by factory method
+      seq_item_port.get_next_item(wr_trans);		// getting generated stimuli grom generator
       
-      write_sample_stimuli(vintf);			// calling task to sample the output from dut
+      drive_write_domain_stimuli(wr_trans);			// calling task to pass stimuli to dut
       
-      wr_analysis_port.write(wr_trans);		// passing transaction to the TLM port 
+      seq_item_port.item_done();
+      
     end
   endtask
+  
+  // driving the generated stimuli to the dut
+  task drive_write_domain_stimuli(async_fifo_wr_transaction wr_tr);
     
-  task write_sample_stimuli(virtual intf #(WIDTH, DEPTH) vintf);
-    @(vintf.wr_cb_sample);
+    @(vintf.wr_cb_drive);
     
-    wr_trans.wr_rst 	= vintf.wr_cb_sample.wr_rst;
-    wr_trans.wr_en 		= vintf.wr_cb_sample.wr_en;
-    wr_trans.data_in	= vintf.wr_cb_sample.data_in;
-    wr_trans.full		= vintf.wr_cb_sample.full;
+    vintf.wr_cb_drive.wr_rst 	<= wr_tr.wr_rst;
+    vintf.wr_cb_drive.wr_en	    <= wr_tr.wr_en;
+    vintf.wr_cb_drive.data_in   <= wr_tr.data_in;
     
-    `uvm_info(get_type_name(), $sformatf("%0s", wr_trans.convert2string()), UVM_LOW);
-    
+    `uvm_info(get_type_name(), $sformatf("%0s", wr_tr.convert2string()), UVM_LOW);
   endtask
   
 endclass
